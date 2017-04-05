860206e4656c230f3b48d8d2e6bd0455d9f80c55
Jeff.Tsai <wcai@maxleap.com>
2017-04-05 18:50:04 +0800

    这个人很懒, 向你丢了一只随机皮卡丘(战斗力: 14448)!

--- raptor-server/src/test/kotlin/as/leap/raptor/server/RaptorOptionsTest.kt
+++ raptor-server/src/test/kotlin/as/leap/raptor/server/RaptorOptionsTest.kt
@@ -1,7 +1,10 @@
 package `as`.leap.raptor.server
 
+import io.vertx.core.Vertx
+import io.vertx.kotlin.core.http.RequestOptions
 import org.testng.Assert
 import org.testng.annotations.Test
+import java.util.concurrent.CountDownLatch
 
 class RaptorOptionsTest {
 
@@ -11,6 +14,23 @@ class RaptorOptionsTest {
     val development = RaptorOptions.development
     Assert.assertNotNull(development)
     Assert.assertNotNull(production)
+  }
+
+  @Test
+  fun test2() {
+    val cdl = CountDownLatch(1)
+    val vertx = Vertx.vertx()
+    val c = vertx.createHttpClient()
+    val requestOptions = RequestOptions("http://baidu.com/")
+    c.getNow(requestOptions, {
+      println(it.statusCode())
+      cdl.countDown()
+    })
+    cdl.await()
+
+
+
+
   }
 
 }
